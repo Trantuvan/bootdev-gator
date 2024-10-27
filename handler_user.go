eba764f63adcd5fe2048a7630812ace3a6202cc4
trantuvan <trantuvan.kan@gmail.com>
2024-10-27 21:29:55 +0700

    refactor delete user error wrap

--- handler_user.go
+++ handler_user.go
@@ -55,7 +55,7 @@ func handlerReset(state *state, command command) error {
 	err := state.db.DeleteUsers(context.Background())
 
 	if err != nil {
-		return err
+		return fmt.Errorf("couldn't delete users %w", err)
 	}
 
 	fmt.Println("reset users tbl successful!")
