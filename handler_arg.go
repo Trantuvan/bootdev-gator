2f16a9e68793b7ceb01b36f45f98663dd1617fc1
trantuvan <trantuvan.kan@gmail.com>
2024-11-18 13:38:54 +0700

    fetched feeds from db between time duration

--- handler_arg.go
+++ handler_arg.go
@@ -3,17 +3,55 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/trantuvan/bootdev-gator/internal/rssfeed"
 )
 
 func handlerAgg(state *state, command command) error {
-	feed, err := rssfeed.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-
-	if err != nil {
-		return fmt.Errorf("failed to fetch RssFeed %w", err)
+	if len(command.args) < 1 || len(command.args) >= 2 {
+		return fmt.Errorf("command %s: takes 1 <time_between_reqs>", command.name)
 	}
 
-	fmt.Printf("%+v\n", *feed)
+	timeBetweenRequests, err := time.ParseDuration(command.args[0])
+
+	if err != nil {
+		return fmt.Errorf("command %s: duration wrong format %w", command.name, err)
+	}
+
+	fmt.Printf("Collecting feeds every %s...", timeBetweenRequests)
+
+	for range time.Tick(timeBetweenRequests) {
+		scrapeFeeds(state)
+	}
+
+	return nil
+}
+
+func scrapeFeeds(state *state) error {
+	ctx := context.Background()
+	nextFeed, err := state.db.GetNextFeedToFetch(ctx)
+
+	if err != nil {
+		return fmt.Errorf("scrapeFeeds: cannot fetch next feed %w", err)
+	}
+
+	_, err = state.db.MarkFeedFetched(ctx, nextFeed.ID)
+
+	if err != nil {
+		return fmt.Errorf("scrapeFeeds: cannot mark feed is fetched %w", err)
+	}
+
+	feedData, err := rssfeed.FetchFeed(ctx, nextFeed.Url)
+
+	if err != nil {
+		return fmt.Errorf("scrapeFeeds: cannot get feed from url:%s -> %w", nextFeed.Url, err)
+	}
+
+	for _, item := range feedData.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+
+	fmt.Printf("Feed %s collected, %v posts found", nextFeed.Name, len(feedData.Channel.Item))
 	return nil
 }
